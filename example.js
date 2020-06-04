bfc00ca4dfa0a97be415fb5d04b2b7b202474837
Mo Morsi <mo@morsi.org>
2020-06-03 21:41:45 -0400

    Add example / demo script

---
+++ example.js
@@ -0,0 +1,67 @@
+const VEight = require('./lib/index').VEight
+const v8 = new VEight()
+
+// Primitive types:
+
+console.log(v8.execute("1"))
+// # => 1
+
+console.log(v8.execute("'123'"))
+// # => '123'
+
+// Standard javascript functions:
+
+console.log(v8.execute("parseInt('123')"))
+// # => 123
+
+// Arrays and objects:
+
+console.log(v8.execute("[1,'a',3.5]"))
+// # => [1, 'a', 3.5]
+
+console.log(v8.execute("a = {b:2, c:3}"))
+// # => {b: 2, c: 3}
+
+// Errors:
+
+try{
+  v8.execute("s")
+}catch(err){
+  console.error(err)
+  // # => ReferenceError: s is not defined
+}
+
+try{
+  console.log(v8.execute("{"))
+}catch(err){
+  console.error(err)
+  // # => SyntaxError: Unexpected end of input
+}
+
+// State is maintained:
+console.log(v8.execute("variable = 'something'"))
+// # => something
+
+console.log(v8.execute("variable"))
+// # => something
+
+// Invoke 'reset' to restore initial context:
+v8.reset()
+
+try{
+  v8.execute("variable")
+}catch(err){
+  console.error(err)
+  // # => ReferenceError: variable is not defined
+}
+
+// Set timeout to terminate long running scripts:
+
+v8.timeout(1000)
+
+try{
+  console.log(v8.execute("while(true){}"))
+}catch(e){
+  console.log(e)
+  // # => timed out
+}
